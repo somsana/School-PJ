601332aed6bc551183382f058600f3749d49c1ed
Somsana Ninthavong <ac100551@avcol.school.nz>
2020-07-02 14:14:19 +1200

    adding data

---
+++ WebApplication1/Areas/Identity/Data/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespacespace School-PJ4.Areas.Identity.Data
+    public class ApplicationUser : IdentityUser
+{
+    [PersonalData]
+    [column(TypeName - "nvarchar(100)")]
+    pulic string FirstName { get; set; }
+
+    [PersonalData]
+    [column(TypeName - "nvarchar(100)")]
+    pulic string LastName { get; set; }
+
+
+
+}
